ff0f9e832bd2b3505600a854316fb7a7bcaad379
Labtgit <80183751+Labtgit@users.noreply.github.com>
2022-04-05 10:50:36 -0600

    Documento esquema para empezar

---
+++ Proyecto F$4.py
@@ -0,0 +1,4 @@
+Se le solicita crear una calculadora de áreas en Python para las siguientes figuras geométricas, que muestre al usuario un menú de la siguiente forma.
+
+username = input("Enter username:")
+print("username is: " + username)
